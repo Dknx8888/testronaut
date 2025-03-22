5cca8ec48fed0f5872b0fed8bc08c99582cfbfff
= <akashsarada@gmail.com>
2025-03-22 14:35:21 -0400

    Updated test cases. Works for files

--- templates.py
+++ templates.py
@@ -7,17 +7,21 @@ def code_refactorting(files):
         text = f.read()
 
 def test_cases(file):
-    #TODO: Implement test cases prompt for Gemini
     with open(file, "r") as f:
         text = f.read()
-        print(text)
 
     prompt = (f"Here is my code. Write me test cases. Make sure that only one file is generated, and that file only has test cases. "
-              f"DO NOT have the original code in the file. Make sure that the cases compile and run correctly."
+              f"DO NOT have any of the original code in the file. Make sure that the cases compile and run correctly."
               f"{text}")
     response = gemini_api.get_gemini_response(prompt)
+
     with open(f"test_cases_{os.path.basename(file)}", "w") as f:
-        f.write(response)
+        for line in response.split("\n"):
+            if line == "```" or line == "```python":
+                continue
+            else:
+                f.write(line + "\n")
+    print("Generated Test Cases. Check the file test_cases_" + os.path.basename(file) + ". \nRemember to review the test cases before running them.")
 
 def code_analysis(files):
     #TODO: Implement code analysis prompt for Gemini
