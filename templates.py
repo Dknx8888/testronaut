b05c4a5f95ac28ba7ae59bf22f5646364b9a8708
= <akashsarada@gmail.com>
2025-03-22 12:52:33 -0400

    added structure of templates file

---
+++ templates.py
@@ -0,0 +1,8 @@
+def code_refactorting(files):
+    #TODO: Implement code refactoring prompt for Gemini
+
+def test_cases(file):
+    #TODO: Implement test cases prompt for Gemini
+
+def code_analysis(files):
+    #TODO: Implement code analysis prompt for Gemini
